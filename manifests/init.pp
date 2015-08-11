5e1e51207dda8ddf1c9da34af1bed3712cf5b290
Julien Pivotto <roidelapluie@inuits.eu>
2015-08-11 23:56:27 +0200

    Implement pipeline

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,5 +1,30 @@
-class profile_jjb{
+class profile_jjb(
+  String $pipeline,
+  String $pipeline_path = '/git',
+  String $tmp_path = '/var/tmp/jjb',
+) {
 
+  package {
+    'git':
+      ensure => installed,
+  }
 
+  include jjb
+
+  Package['git'] -> Class['jjb::python']
+
+  vcsrepo {
+    "${tmp_path}/${pipeline}":
+      ensure   => latest,
+      provider => 'git',
+      source   => "${pipeline_path}/${pipeline}",
+  }
+  ~>
+  exec { "Pipeline $pipeline":
+    command     => "jenkins-jobs update ${tmp_path}/${pipeline}",
+    path        => '/usr/bin:/usr/sbin:/bin:/usr/local/bin',
+    refreshonly => true,
+    require     => Class['profile_jenkins', 'jjb'],
+  }
 
 }
